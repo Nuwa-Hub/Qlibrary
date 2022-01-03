00c6872eaa162b048dfc77cbcb0009348900fa86
Nuwandeshapriya <79309450+Nuwandeshapriya@users.noreply.github.com>
2022-01-03 07:20:50 +0530

    keyword

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="utf-8">
+    <meta name="keywords" content="Qlibrary, combined math, CM">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Q Library| Home</title>
     <link rel="stylesheet" href="css/test.css">
